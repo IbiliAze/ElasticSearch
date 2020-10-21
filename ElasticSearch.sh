079c9265b5f2cd80dd3d4f64cf009f6b0b74b033
Ibi Hasanli <ibili73@gmail.com>
2020-10-21 06:06:10 -0700

    PKI

--- ElasticSearch.sh
+++ ElasticSearch.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 
+[ Test Nodes & Cluster ]
+
 curl localhost:9200/_cat
 
 curl localhost:9200/_cat/nodes?v #verbose
+
+
+
+[ PKI ]
+
+sudo /usr/share/elasticsearch/bin/elasticsearch-certutil ca --out /etc/elasticsearch/ca --pass elasticsearch_cai #CA
+sudo /usr/share/elasticsearch/bin/elasticsearch-certutil cert --ca /etc/elasticsearch/ca --ca-pass elasticsearch_ca  --name master1 --dns ip-172-31-72-12.ec2.internal --ip 172.31.72.12 --out /etc/elasticsearch/master1 --pass elastic_master1
+sudo ls /etc/elasticsearch
+sudo /usr/share/elasticsearch/bin/elasticsearch-certutil cert --ca /etc/elasticsearch/ca --ca-pass elasticsearch_ca  --name data1 --dns ip-172-31-39-228.ec2.internal --ip 172.31.39.228 --out /etc/elasticsearch/data1 --pass elastic_data1
+sudo mv /etc/elasticsearch/data1 /tmp
+sudo chown $USER:$USER /tmp/data1
+
+
+
